71d27c557a71f69550703c7b48b12d15f5474d0b
Gabriel Oliveira <gabriel.oliveira@axented.com>
2021-11-09 20:56:21 -0600

    Finished project

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,40 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: MagicBall(),
       ),
     );
+
+class MagicBall extends StatefulWidget {
+  @override
+  _MagicBallState createState() => _MagicBallState();
+}
+
+class _MagicBallState extends State<MagicBall> {
+  int ballNumber;
+  @override
+  Widget build(BuildContext context) {
+    changeBallNumber();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Ask Me Anything"),
+        backgroundColor: Colors.blue.shade900,
+      ),
+      body: Center(
+          child: TextButton(
+        onPressed: () => changeBallNumber(),
+        child: Image.asset('images/ball$ballNumber.png'),
+      )),
+      backgroundColor: Colors.blue,
+    );
+  }
+
+  void changeBallNumber() {
+    setState(() {
+      ballNumber = Random().nextInt(5) + 1;
+    });
+  }
+}
